e8d84a2c6b812069366d88d4177b4dee84fc80af
maldoalex <email@address.com>
2019-06-19 15:16:13 -0500

    afternoon project complete

--- practice.js
+++ practice.js
@@ -15,6 +15,11 @@
 */
 
 //Code here
+let me = {
+  name: "Alex",
+  age: 37
+};
+// alert(me.name);
 
 ////////// PROBLEM 2 //////////
 
@@ -24,6 +29,14 @@
 */
 
 //Code here
+let favoriteThings = {
+  band: "G&R",
+  food: "Japanese",
+  person: "mom",
+  book: "The Bible",
+  movie: "Predator",
+  holiday: "Christmas"
+};
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
@@ -31,13 +44,16 @@
 */
 
 //Code here
-
+favoriteThings.car = "Viper";
+favoriteThings.brand = "Thorogood";
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 ////////// PROBLEM 3 //////////
 
@@ -50,12 +66,16 @@
 */
 
 //Code here
+let backPack = {};
+let item = "firstPocket";
+backPack[item] = "chapstick";
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
+alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -64,17 +84,17 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log(backPack);
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
 var user2 = {
-  name: 'Bryan',
+  name: "Bryan",
   age: 24,
-  pwHash: 'U+Ldlngx2BYQk',
-  email: 'BryanSmith33@gmail.com',
-  birthday: '05/02/1990',
-  username: 'bryansmith33'
+  pwHash: "U+Ldlngx2BYQk",
+  email: "BryanSmith33@gmail.com",
+  birthday: "05/02/1990",
+  username: "bryansmith33"
 };
 // Do not edit the code above.
 
@@ -85,6 +105,8 @@ var user2 = {
 */
 
 //Code Here
+user2.name = "Bryan G. Smith";
+user2.email = "bryan.smith@devmounta.in";
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -96,6 +118,7 @@ var user2 = {
 */
 
 //Code Here
+let methodCollection = {};
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
@@ -103,7 +126,13 @@ var user2 = {
 */
 
 //Code Here
+methodCollection.alertHello = function() {
+  alert("hello");
+};
 
+methodCollection.logHello = function() {
+  console.log("hello");
+};
 /*
   Now call your alertHello and logHello methods.
 */
@@ -118,6 +147,14 @@ var user2 = {
 */
 
 //Code Here
+function makePerson(name, birthday, ssn) {
+  let obj = {
+    name: name,
+    birthday: birthday,
+    ssn: ssn
+  };
+  return obj;
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -127,3 +164,11 @@ var user2 = {
 */
 
 //Code Here
+function makeCard(cardNumber, expirationDate, securityCode) {
+  let creditCard = {
+    cardNumber: cardNumber,
+    expirationDate: expirationDate,
+    securityCode: securityCode
+  };
+  return creditCard;
+}
